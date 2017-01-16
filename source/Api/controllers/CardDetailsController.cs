13c75d4b6ff15c4da2479c849f5ca9144f7559cd
Vichitra Samarawickrama <vichitra.sam91@gmail.com>
2017-01-16 10:29:55 +0530

    CardDetailsController.cs file updated

--- source/Api/controllers/CardDetailsController.cs
+++ source/Api/controllers/CardDetailsController.cs
@@ -5,65 +5,45 @@ using MySql.Data.MySqlClient;
 using Card.Entity;
 using Newtonsoft.Json;
 using Card.Models;
+using System.Linq;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
 namespace Card.Controllers
 {
     [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true, Duration = -1)]
-    [Route("api/CardRequest/id")]
+    [Route("api/[action]")]
     public class CardController : Controller
     {
         private static IDbRepository<CardRequest> CardRequestDbRepository { get; set; }
         [HttpPost]
         public IActionResult CardRequestWithID([FromBody] CardRequest cardRequest, IDbRepository<CardRequest> iDbRepository = null)
         {
-
-            CardRequest cardRequestAsigned = null;
-            
             try
             {
                 IEnumerable<CardRequest> cardDetailsList = CardRequestDbRepository.GetAll();
-                cardRequest cardDetails = cardDetailsList.Where(x=>x.Id == cardRequest.Id).FirstOrDefault();
+                cardRequest = cardDetailsList.Where(x=>x.Id == cardRequest.Id).FirstOrDefault();
 
-                Ok(cardDetails);
+                return Ok(cardRequest);
             }
-            catch (Exception e)
+            catch (MySqlException mySqlException)
             {
                 return BadRequest(mySqlException.Message);
             }
-
-            return JsonConvert.SerializeObject(new RequestResult
-            {
-                State = RequestState.Success,
-                Data = new
-                {
-                    Id = cardRequestAsigned.Id,
-                    CardNo = cardRequestAsigned.CardNo,
-                    TemplateId = cardRequestAsigned.TemplateId,
-                    CardHoldeName = cardRequestAsigned.CardHoldeName,
-                    ExpireDate = cardRequestAsigned.ExpireDate
-                }
-            });
-
-
         }
 
         [HttpPost]
-        public bool CardRequest([FromBody]CardRequest cardRequest)
+        public IActionResult CardRequest([FromBody]CardRequest cardRequest,IDbRepository<CardRequest> iDbRepository = null)
         {
-
-            string query = "insert into card_request (id,card_no,template_id,card_holder_name,expire_date) values(" + cardRequest.Id + "," + cardRequest.CardNo + "," + cardRequest.TemplateId + ",'" + cardRequest.CardHoldeName + "','" + cardRequest.ExpireDate + "');";
-
             try
             {
-                using (DbConnect connect = new DbConnect())
-                {
-                    connect.MysqlExecuteNonQuery(query);
-                }
+                CardRequestDbRepository.Add(cardRequest);
+                return Ok(true);
             }
-            catch (Exception e)
+            catch (MySqlException mySqlException)
             {
-                return false;
+                return BadRequest(mySqlException.Message);
             }
-            return true;
         }
     }
 }
